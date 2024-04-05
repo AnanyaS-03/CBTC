46a3594c92b9f6c045d5d00605efdfa2d56e8ae1
AnanyaS-03 <166102243+AnanyaS-03@users.noreply.github.com>
2024-04-06 00:17:52 +0530

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
-#include<conio.h>
+#include<iostream>                                                                              
+#include<conio.h>                                                             
 using namespace std;
 
-
-int chance , check[9] , z , count=1;
 char a='1' , b='2' , c='3' , d='4' , e='5' , f='6' , g='7' , h='8' , i='9'; 
 
-void input(int a)//passing player number as input argument
+int chance , checking[9] , z , count=1;
+
+
+
+void input(int o)//passing player number as input argument
 {
     //checking winner condition for player 1
-    if((a=='0' && b=='0' && c=='0')||(d=='0' && e=='0' && f=='0')||(g=='0' && h=='0' && i=='0')||(a=='0' && d=='0' && g=='0')||(b=='0' && e=='0' && h=='0')||(c=='0' && f=='0' && i=='0')||(a=='0' && e=='0' && i=='0')||(c=='0' && e=='0' && g=='0')){
+    if((a=='0' && b=='0' && c=='0')||(d=='0' && e=='0' && f=='0')||(g=='0' && h=='0' && i=='0')||(a=='0' && d=='0' && g=='0')
+	||(b=='0' && e=='0' && h=='0')||(c=='0' && f=='0' && i=='0')||(a=='0' && e=='0' && i=='0')||(c=='0' && e=='0' && g=='0')){
                 cout<<" "<<endl;
                 cout<<"PLAYER 1 VICTORY"<<endl;  
                 cout<<" "<<endl;
@@ -18,7 +21,8 @@ void input(int a)//passing player number as input argument
 }
 
     //checking winner condition for player 2
-    if((a=='X' && b=='X' && c=='X'||d=='X' && e=='X' && f=='X'||g=='X' && h=='X' && i=='X'||a=='X' && d=='X' && g=='X'||b=='X' && e=='X' && h=='X'||c=='X' && f=='X' && i=='X'||a=='X' && e=='X' && i=='X'||c=='X' && e=='X' && g=='X')){
+    if((a=='X' && b=='X' && c=='X')||(d=='X' && e=='X' && f=='X')||(g=='X' && h=='X' && i=='X')||(a=='X' && d=='X' && g=='X')
+	||(b=='X' && e=='X' && h=='X')||(c=='X' && f=='X' && i=='X')||(a=='X' && e=='X' && i=='X')||(c=='X' && e=='X' && g=='X')){
                 cout<<" "<<endl;
                 cout<<"PLAYER 2 VICTORY"<<endl;
                 cout<<" "<<endl;
@@ -37,7 +41,7 @@ void input(int a)//passing player number as input argument
 
     //getting input by player
     cout<<" "<<endl;
-    cout<<"PLAYER "<<a<<" TURN: ";
+    cout<<"PLAYER "<<o<<" TURN: ";
     cin>>chance;
     cout<<" "<<endl;
     
@@ -45,20 +49,20 @@ void input(int a)//passing player number as input argument
     if (chance>9||chance<1){
                 cout<<"ENTER VALID NUMBER: "<<endl;
                 count--;
-                input(a);//if input is invalid then calling the input function again
+                input(o);//if input is invalid then calling the input function again
             }
  
     for(z;z<count;z++){
                 
                 //checking if a position entered by player is already taken or not
-                if(check[z]==chance){
+                if(checking[z]==chance){
                     cout<<"POSITION ALREADY TAKEN, ENTER AGAIN: "<<endl;
-                    input(a);
+                    input(o);
                 }
                 
                 else{
 
-                    check[z]=chance;//putting the input value into the array
+                    checking[z]=chance;//putting the input value into the array
                     break;
                 }
                 
@@ -70,7 +74,6 @@ void input(int a)//passing player number as input argument
     
 //creating a function to define the structure of the game
 void setup(){
-	
         cout<<a<<" |"<<b<<" |"<<c<<endl;
         cout<<"__|__|__"<<endl;
         cout<<d<<" |"<<e<<" |"<<f<<endl;
@@ -83,10 +86,9 @@ int main(){
 cout<<"            "<<"********************"<<endl;
 cout<<"            "<<"| TIC TAC TOE GAME |"<<endl;
 cout<<"            "<<"********************"<<endl<<endl;
-cout<<" "<<"* ENTER THE NUMBER WHERE YOU WANT TO PUT X OR O "<<endl;
-cout<<"                                             "<<endl<<endl;
-cout<<"                PLAYER 1= O               PLAYER 2= X"<<endl<<endl;
-cout<<"                           *** LETS PLAY ***                                       "<<endl<<endl;
+cout<<" "<<"* ENTER THE NUMBER WHERE YOU WANT TO DISPLAY X OR 0"<<endl<<endl;
+cout<<"      PLAYER 1= 0               PLAYER 2= X"<<endl<<endl;
+cout<<"              ****** LETS PLAY ******                             "<<endl;
 
 setup();
 
@@ -130,12 +132,12 @@ while(count<=10){
             
             case 7:
             g='X';
-            setup();
+           setup();
             break;
             
             case 8:
             h='X';
-            setup();
+            setup(); 
             break;
             
             case 9:
@@ -194,7 +196,7 @@ while(count<=10){
            
             case 9:
             i='0';
-            setup();
+           setup();
             break;
             
         }
